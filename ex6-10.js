2b4d5a84aead21b6dfefa5602651a9013cd2fc50
kimyamazaki <zm20255@josai.ac.jp>
2022-02-10 18:56:46 +0000

    message

---
+++ ex6-10.js
@@ -0,0 +1,9 @@
+function imgchange1() {
+    document.getElementById('logo').src= 
+    './image/ロゴ旧.png';
+}
+
+function imgchange2() {
+    document.getElementById('logo').src= 
+    './image/現在ロゴ.jpg';
+}
